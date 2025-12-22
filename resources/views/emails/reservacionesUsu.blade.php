8d70f6b66410af3a4d878710d2b6ccde37de6759
Mario_Brnal10 <248516323+ViajeroCarRental@users.noreply.github.com>
2025-12-21 23:00:00 -0600

    logos en los correos

--- resources/views/emails/reservacionesUsu.blade.php
+++ resources/views/emails/reservacionesUsu.blade.php
@@ -90,7 +90,7 @@
 
   <!-- HEADER -->
   <div class="header">
-    <img src="https://i.ibb.co/rR3WvFNf/image.png" alt="Viajero Car Rental">
+    <img src="{{ asset('img/Logo3.jpg') }}" alt="Viajero Car Rental">
     <h1>
       {{ $tipo === 'linea' ? 'Reserva Confirmada' : 'Confirmación de Reservación' }}
     </h1>
