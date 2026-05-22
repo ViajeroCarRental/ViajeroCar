308427e1d20659ebfba7d3cde0752ededc6decb1
Mario_Brnal10 <reservaciones@viajerocarental.com>
2026-05-22 11:13:37 -0600

    .

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,10 @@ use App\Http\Controllers\VehiculoDocumentosController;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\VisorReservacionController;
 use App\Http\Controllers\DropoffController;
+use App\Http\Controllers\AdicionalesController;
+use App\Http\Controllers\DepositoController;
+use App\Http\Controllers\OficinaController;
+
 //rutas vistas Usuario
 
 
